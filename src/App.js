8e69b6f78d88a43fa1350ccdae61b9aa8ace2ea6
mytrofanov <mytrofanovmaxym@gmail.com>
2021-07-15 11:24:28 +0300

    Hook - useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
+    const [counter, setCounter] = useState(0)
+
+    function add  () {
+        setCounter (counter+1)
+    }
+    function reduction () {
+        setCounter(counter-1)
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+  <h1>Счетчик: {counter}</h1>
+      <button onClick={add} className="btn btn-success">Добавить</button>
+      <button onClick={reduction} className="btn btn-danger">Удалить</button>
     </div>
   );
 }
