ba3c0664899d092dac55265d230f06d0e2b0a0fa
mytrofanov <mytrofanovmaxym@gmail.com>
2021-07-15 11:37:05 +0300

    Hook - useState. Обращение к предыдущему состоянию

--- src/App.js
+++ src/App.js
@@ -10,12 +10,16 @@ function App() {
     function reduction () {
         setCounter(counter-1)
     }
-
+    function double () {
+        setCounter(prev => prev +1)
+        setCounter(prev => prev +1) //только обращаясь к предыдущему состоянию можно вызвать useState два раза
+    }
   return (
     <div>
   <h1>Счетчик: {counter}</h1>
       <button onClick={add} className="btn btn-success">Добавить</button>
       <button onClick={reduction} className="btn btn-danger">Удалить</button>
+      <button onClick={double} className="btn btn-warning">Увеличивать по 2</button>
     </div>
   );
 }
